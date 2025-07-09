6b8f20ffc96ddaf278c47389d60175831a432d97
Tammam Bin Masum <tammamxyz@gmail.com>
2025-07-09 19:29:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 def run_quiz():
     questions = [
+{
+            "question": "What is the capital of France?",
+            "options": ["A. Berlin", "B. Madrid", "C. Paris", "D. Rome"],
+            "answer": "C"
